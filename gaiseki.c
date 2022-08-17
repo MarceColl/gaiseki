fe5744fe2934ef4e14ef6b7ec37a76e3b3063ce6
Marce Coll <marce@dziban.net>
2022-08-18 00:30:48 +0200

    resize nodes

--- gaiseki.c
+++ gaiseki.c
@@ -10,8 +10,8 @@
 #define VIRT_SCREEN_WIDTH 500
 #define VIRT_SCREEN_HEIGHT 500
 
-#define PX_WIDTH REAL_SCREEN_WIDTH / VIRT_SCREEN_WIDTH
-#define PX_HEIGHT REAL_SCREEN_HEIGHT / VIRT_SCREEN_HEIGHT
+#define PX_WIDTH (uint32_t)(REAL_SCREEN_WIDTH / VIRT_SCREEN_WIDTH)
+#define PX_HEIGHT (uint32_t)(REAL_SCREEN_HEIGHT / VIRT_SCREEN_HEIGHT)
 
 typedef SDL_Surface S;
 
@@ -23,7 +23,13 @@ typedef enum {
 typedef struct {
   uint32_t x;
   uint32_t y;
+  float sz;
 } P;
+
+typedef struct {
+  float x;
+  float y;
+} Pf;
 
 typedef struct {
   P *p;
@@ -62,13 +68,33 @@ void initApp(A *app) {
 }
 
 // paint pixel
-void pxp(SDL_Surface *s, uint64_t x, uint64_t y, uint8_t r, uint8_t g,
-         uint8_t b) {
-  for (int dy = 0; dy < PX_HEIGHT; ++dy) {
-    for (int dx = 0; dx < PX_WIDTH; ++dx) {
+void pxp(S *s, uint64_t x, uint64_t y, uint8_t r, uint8_t g, uint8_t b,
+         float sz) {
+  for (uint32_t dy = 0; dy < PX_HEIGHT * sz; ++dy) {
+    for (uint32_t dx = 0; dx < PX_WIDTH * sz; ++dx) {
       *((int *)s->pixels + (x * PX_WIDTH + dx) + (y * PX_HEIGHT + dy) * s->w) =
           SDL_MapRGB(s->format, r, g, b);
     }
+  }
+}
+
+// paint pixel
+void upxp(S *s, uint64_t x, uint64_t y, uint8_t r, uint8_t g, uint8_t b) {
+  pxp(s, x, y, r, g, b, 1);
+}
+
+void d_l(S *s, P *p1, P *p2, uint8_t r, uint8_t g, uint8_t b, uint8_t sz) {
+  int32_t dx = p2->x - p1->x;
+  int32_t dy = p2->y - p1->y;
+
+  float len = sqrt(dx * dx + dy * dy);
+  Pf dvl = {.x = (float)dx / len, .y = (float)dy / len};
+  Pf cp = {.x = (float)p1->x, .y = (float)p1->y};
+
+  while (fabs(p2->x - cp.x) > 0.5f && fabs(p2->y - cp.y) > 0.5f) {
+    pxp(s, round(cp.x), round(cp.y), r, g, b, sz);
+    cp.x += dvl.x;
+    cp.y += dvl.y;
   }
 }
 
@@ -77,7 +103,7 @@ void d_bg(S *s) {
   memset((uint32_t *)s->pixels, 0, s->w * sizeof(uint32_t) * s->h);
   for (int x = 15; x < VIRT_SCREEN_WIDTH; x += 20) {
     for (int y = 15; y < VIRT_SCREEN_HEIGHT; y += 20) {
-      pxp(s, x, y, 80, 80, 80);
+      upxp(s, x, y, 80, 80, 80);
     }
   }
 }
@@ -87,17 +113,19 @@ void d_cur(S *s, uint64_t cx, uint64_t cy) {
   for (int x = -5; x <= 5; x++) {
     int32_t tpx = cx + x;
     if (tpx >= 0 && tpx < VIRT_SCREEN_WIDTH) {
-      pxp(s, cx + x, cy, 200, 200, 200);
+      upxp(s, cx + x, cy, 200, 200, 200);
     }
   }
 
   for (int y = -5; y <= 5; y++) {
     int32_t tpy = cy + y;
     if (tpy >= 0 && tpy < VIRT_SCREEN_HEIGHT) {
-      pxp(s, cx, cy + y, 200, 200, 200);
+      upxp(s, cx, cy + y, 200, 200, 200);
     }
   }
 }
+
+float flerp(float f1, float f2, double t) { return f1 + t * (f2 - f1); }
 
 // linear interpolation
 P lerp(P *p1, P *p2, double t) {
@@ -131,9 +159,9 @@ P rlerp(P *p, int np, double t) {
 }
 
 void d_dl(S *s, P *p1, P *p2, uint8_t r, uint8_t g, uint8_t b) {
-  for (float t = 0; t < 1.0f; t += 0.01f) {
+  for (double t = 0; t < 1.0; t += 0.01) {
     P p = lerp(p1, p2, t);
-    pxp(s, p.x, p.y, r, g, b);
+    upxp(s, p.x, p.y, r, g, b);
   }
 }
 
@@ -148,7 +176,7 @@ void d_c(A *a, C *c) {
         if (a->hover == p) {
           h = 255;
         }
-        pxp(s, p->x + dx, p->y + dy, h, h, h);
+        upxp(s, p->x + dx, p->y + dy, h, h, h);
       }
     }
   }
@@ -156,18 +184,35 @@ void d_c(A *a, C *c) {
   if (c->np < 2)
     return;
 
-  for (float t = 0; t <= 1.0f; t += 0.001f) {
-    P p = rlerp(c->p, c->np, t);
-    pxp(s, p.x, p.y, 255, 0, 0);
+  P p = rlerp(c->p, c->np, 0);
+  for (double t = 0; t <= 1.0; t += 0.001) {
+    P np = rlerp(c->p, c->np, t);
+    float sz = flerp(c->p[0].sz, c->p[c->np - 1].sz, t);
+    d_l(s, &p, &np, 255, 0, 0, sz);
+    p = np;
+    pxp(s, p.x, p.y, 255, 0, 0, sz);
   }
 
   if (c->np == 3) {
     for (int i = 0; i < c->np - 1; i++) {
-      d_dl(s, &c->p[i], &c->p[i + 1], 100, 100, 100);
+      d_l(s, &c->p[i], &c->p[i + 1], 100, 100, 100, 1);
     }
   } else if (c->np > 3) {
     for (int i = 0; i < c->np - 1; i += 2) {
-      d_dl(s, &c->p[i], &c->p[i + 1], 100, 100, 100);
+      d_l(s, &c->p[i], &c->p[i + 1], 100, 100, 100, 1);
+    }
+  }
+
+  for (int i = 0; i < c->np; ++i) {
+    P *p = &c->p[i];
+    for (int dx = -2; dx <= 2; dx++) {
+      for (int dy = -2; dy <= 2; dy++) {
+        uint8_t h = 128;
+        if (a->hover == p) {
+          h = 255;
+        }
+        upxp(s, p->x + dx, p->y + dy, h, h, h);
+      }
     }
   }
 }
@@ -199,23 +244,48 @@ int main() {
     if (SDL_QUIT == event.type) {
       exit(0);
     } else if (SDL_MOUSEBUTTONDOWN == event.type) {
+      if (app.hover) {
+        app.t = MOVE_POINT;
+      }
+
       int cx = div(event.button.x, PX_WIDTH).quot;
       int cy = div(event.button.y, PX_HEIGHT).quot;
-      P *np = c_ap(&app.c);
-      np->x = cx;
-      np->y = cy;
+      if (app.t == ADD_POINT) {
+        P *np = c_ap(&app.c);
+        np->x = cx;
+        np->y = cy;
+        np->sz = 4;
+      }
     } else if (SDL_MOUSEBUTTONUP == event.type) {
       if (app.t == MOVE_POINT) {
         app.t = ADD_POINT;
+      }
+    } else if (SDL_MOUSEWHEEL == event.type) {
+      printf("WASSUP\n");
+      if (app.hover) {
+        printf("%d\n", event.wheel.y);
+        if (event.wheel.y > 0) {
+          ((P *)app.hover)->sz += 0.5f;
+          printf("?? %f\n", ((P *)app.hover)->sz);
+        } else if (event.wheel.y < 0) {
+          ((P *)app.hover)->sz -= 0.2f;
+        }
       }
     } else if (SDL_MOUSEMOTION == event.type) {
       int cx = div(event.button.x, PX_WIDTH).quot;
       int cy = div(event.button.y, PX_HEIGHT).quot;
 
-      for (int i = 0; i < app.c.np; i++) {
-        P *ip = &app.c.p[i];
-        if (ip->x == cx && ip->y == cy) {
-          app.hover = ip;
+      if (app.hover && app.t == MOVE_POINT) {
+        ((P *)app.hover)->x = cx;
+        ((P *)app.hover)->y = cy;
+      } else {
+        app.hover = NULL;
+        for (int i = 0; i < app.c.np; i++) {
+          P *ip = &app.c.p[i];
+          if (ip->x >= (cx - PX_WIDTH) && ip->x <= (cx + PX_WIDTH) &&
+              ip->y >= (cy - PX_WIDTH) && ip->y <= (cy + PX_WIDTH)) {
+            app.hover = ip;
+          }
         }
       }
 
